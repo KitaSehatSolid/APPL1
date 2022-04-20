cecd23bb20c1baeb70f161f62f6bba621d103109
Zlarex <ihsanfauzan76@gmail.com>
2022-04-20 17:04:44 +0700

    fix schema

--- schemas/task.js
+++ schemas/task.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
     server_id: String,
+    task_id: String,
     kode_matkul: String,
-    description: String
+    description: String,
+    accept: Array,
+    decline: Array
 }, {versionKey: false})
 
 module.exports = mongoose.models.Config || mongoose.model('Task', TaskSchema)
